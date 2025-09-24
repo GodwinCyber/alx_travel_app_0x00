a1f9dce7a08725573297b619dd0698a6834fbfd9
GodwinCyber <godwinchuks032@gmail.com>
2025-09-24 17:37:49 +0100

    Created database model, serialize it and seed it

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external apps
+    'rest_framework',
+    'django_seed',
+
+    # local apps
+    'listings',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'listings.User'
+
